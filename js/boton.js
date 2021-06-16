fdc703ef291ac7248888cd41908c04de93d24b67
GL Videos <glfotosvideos@gmail.com>
2021-06-16 00:41:04 -0300

    Se modifico el botón flotante

--- js/boton.js
+++ js/boton.js
@@ -1,7 +1,7 @@
 (function() {
     //a partir de que punto del scroll vertical de la ventana mostrará el botón
     const ishow = 800
-    const $divtop = document.getElementById("div-totop")
+    const $divtop = document.getElementById("btn-home")
     window.addEventListener("scroll", function() {
         if(document.documentElement.scrollTop > ishow){
             $divtop.style.display = "inherit"
